2e91d252d659c2fa8c93c0adfd1b03e9744bbc95
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-06 11:41:17 +0000

    feat(isnotnull): add isNotNull function

--- index.ts
+++ index.ts
@@ -4,3 +4,10 @@
 export function isNull<T>(value: unknown): value is null | undefined {
     return value == null;
 }
+
+/** Tests if the specified value is null or undefined.
+ *
+ * Useful as the predicate of filter functions and similar. */
+export function isNotNull<T>(value: T | null | undefined): value is T {
+    return value != null;
+}
